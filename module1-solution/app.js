ac93c9514ad372faef23526b3eac284d639edb0b
nishant <nishantsaraswat100@gmail.com>
2017-03-15 13:19:07 -0700

    test

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,11 +9,14 @@ function LunchCheckController($scope)
 {
 $scope.message="";
 $scope.foodItem="";
+
 $scope.lunchCheck=function(){
+
 if($scope.foodItem!=''){
 var arr=($scope.foodItem).split(',');
+var size = arr.filter(function(value) { return (value != undefined && value != '' ) }).length;
 $scope.myStyle={color:"green"};
-if(arr.length>3){
+if(arr.length>3 && size>3){
 $scope.message="Too much!";
 }
 else
